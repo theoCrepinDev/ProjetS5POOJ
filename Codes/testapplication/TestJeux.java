be037c4cea759a45d6d6869bffae60f90fd5d96a
Burette <95096139+JulesBuretteDev@users.noreply.github.com>
2021-12-11 22:48:44 +0100

    interface ( j'ai du deja l'envoye normalement)

--- Codes/testapplication/TestJeux.java
+++ Codes/testapplication/TestJeux.java
@@ -4,18 +4,21 @@ import application.Jouer;
 import application.Joueurs;
 
 public class TestJeux {
-        public static void main(String[] args){
-            CreationDonnees.creer10Themes();
-            CreationDonnees.creationQuestions();
+    public static void lanceJeu(){
+        CreationDonnees.creer10Themes();
+        CreationDonnees.creationQuestions();
 
-            Joueurs joueursTest = new Joueurs();
-            joueursTest.generation20Joueurs();
+        Joueurs joueursTest = new Joueurs();
+        joueursTest.generation20Joueurs();
 
-            Jouer jeu1 = new Jouer();
-            jeu1.jouerPhase1();
-            
-            
-            System.out.println(joueursTest.getJoueur());
+        Jouer jeu1 = new Jouer();
+        jeu1.jouerPhase1();
+        
+        
+        System.out.println(joueursTest.getJoueur());
+    }
 
-        }
+    public static void main(String[] args){
+        lanceJeu();
+    }
 }
