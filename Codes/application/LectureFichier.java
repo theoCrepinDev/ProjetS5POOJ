17da220d33c097d8fb9365999b47c09d20b8dd1d
Burette <95096139+JulesBuretteDev@users.noreply.github.com>
2021-12-12 15:24:06 +0100

    debut d'ajout du menu developper

---
+++ Codes/application/LectureFichier.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.io.*;
+import java.util.Scanner; 
+
+
+public class LectureFichier {
+    /*
+    public static String[][] LectureQuestion() throws FileNotFoundException{
+        FileInputStream file = new FileInputStream("Donnees.txt");
+        Scanner scanner = new Scanner(file);
+
+        //Lecture du nombre de sommets
+        String ligne = scanner.nextLine();
+        int nbrElements = Integer.parseInt(ligne);
+
+        //Initialisation du tableau de la matrice d'adjacence
+        //De taille nbrSommets,nbrSommets
+        double[][] donnees = new double[nbrElements][nbrSommets];
+
+        scanner.nextLine();
+
+        //Initialisation de toutes les valeurs du tableau à inf
+        for(int i = 0; i < nbrSommets; i++){
+            for(int j = 0; j < nbrSommets; j++){
+                matriceAdjacence[i][j] = inf;
+             }
+        }
+
+        while (scanner.hasNextLine()){
+            String lineString = scanner.nextLine();
+            //On récupère le tableau des Trois valeures séparés en String
+            String[] lineSepString = separation(lineString);
+            //On met les valeurs en int dans des variables
+            int k = Integer.parseInt(lineSepString[0]);
+            int l = Integer.parseInt(lineSepString[1]);
+            double cout = Double.parseDouble(lineSepString[2]);
+            matriceAdjacence[k][l] = cout;
+        }
+
+        scanner.close();
+        return matriceAdjacence;
+    }
+    */
+    
+}
